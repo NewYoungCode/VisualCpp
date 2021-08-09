2e0aa6cf7e37043975d96bc96de42da02d0ace39
home-pc <718987717@qq.com>
2021-08-09 20:02:01 +0800

    1

--- main.cpp
+++ main.cpp
@@ -8,7 +8,33 @@
 #include "Text.hpp"
 #include "WinTool.hpp"
 
+
+class aa {
+public:
+	
+	void print() {
+		std::cout << value;
+		hell0();
+	}
+	void hell0() {
+	
+	}
+	aa(int v) :value(v) {
+
+	}
+private:
+	aa() {
+		value = 10;
+	}
+	int value;
+};
+
+
 int main(int count, char*args[]) {
+
+	aa a;
+	aa c(10);
+
 	auto a = WinTool::FindProcessId("WeChatApp.exe").at(0);
 	auto b = WinTool::FindProcessFilename(a);
 	WinTool::EnablePrivilege();
