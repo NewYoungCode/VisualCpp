d097aa1e2c1e8f0a87965d08ba0777e0950dd606
Administrator <Administrator@DESKTOP-SIITC17>
2021-08-18 18:02:53 +0800

    1

--- include/Time.hpp
+++ include/Time.hpp
@@ -5,13 +5,13 @@ namespace Time {
 		inline  std::string ToString(const std::string &format = "yyyy-MM-dd hh:mm:ss") {
 			SYSTEMTIME time;
 			GetLocalTime(&time);
-			std::string  year = std::to_string((WORD)time.wYear);//年
-			std::string  Month = std::to_string((WORD)time.wMonth);//月
-			std::string  Day = std::to_string((WORD)time.wDay);//日
-			std::string Hour = std::to_string((WORD)time.wHour);//时
-			std::string Minute = std::to_string((WORD)time.wMinute);//分
-			std::string Second = std::to_string((WORD)time.wSecond);//秒
-			std::string wMilliseconds = std::to_string((WORD)time.wMilliseconds);//毫秒
+			std::string  year = std::to_string(time.wYear);//年
+			std::string  Month = std::to_string(time.wMonth);//月
+			std::string  Day = std::to_string(time.wDay);//日
+			std::string Hour = std::to_string(time.wHour);//时
+			std::string Minute = std::to_string(time.wMinute);//分
+			std::string Second = std::to_string(time.wSecond);//秒
+			std::string wMilliseconds = std::to_string(time.wMilliseconds);//毫秒
 			Month = Month.size() == 1U ? "0" + Month : Month;
 			Day = Day.size() == 1U ? "0" + Day : Day;
 			Hour = Hour.size() == 1U ? "0" + Hour : Hour;
